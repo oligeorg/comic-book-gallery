41e179530de26a8deb73f9800914eca4f78af2a8
Olafur Georg Gylfason <ogg4@hi.is>
2019-03-07 19:58:15 +0000

    Added Comic Books List view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -59,6 +59,11 @@ namespace ComicBookGallery.Data
                 Favorite = false
             }
         };
+        
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
 
         public ComicBook GetComicBook(int id)
         {
